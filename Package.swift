437194e5b3704fad68ad7975cb6ca295c12cdeb4
Mikhail Nikanorov <fobozsb@gmail.com>
2020-11-17 00:48:19 -0800

    package update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.0.0")),
+    // CryptoSwift moved to xcframework
+    // we should use exact version, until fix
+    // https://bugs.swift.org/browse/SR-13345
+    // https://forums.swift.org/t/swiftpm-binarytarget-dependency-and-code-signing/38953/40
+    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.3.3")),
     .package(url: "https://github.com/MyEtherWallet/MEW-wallet-iOS-secp256k1-package.git", from: "1.0.0"),
     .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
     .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0"))
