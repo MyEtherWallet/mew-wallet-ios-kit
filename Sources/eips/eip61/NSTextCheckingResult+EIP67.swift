bf469a1a249805b90313c191a1e5d4fe450c087c
Misha Nikanorov <fobozsb@gmail.com>
2021-09-11 02:27:16 -0700

    feature/EIP-67: Fix misname eip61 -> eip67

--- Sources/eips/eip61/NSTextCheckingResult+EIP67.swift
+++ Sources/eips/eip61/NSTextCheckingResult+EIP67.swift
@@ -1,5 +1,5 @@
 //
-//  NSTextCheckingResult+EIP61.swift
+//  NSTextCheckingResult+EIP67.swift
 //  MEWwalletKit
 //
 //  Created by Mikhail Nikanorov on 9/11/21.
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum EIP61Groups: String {
+enum EIP67Groups: String {
   case target
   case parameters
 }
@@ -17,18 +17,18 @@ internal extension NSTextCheckingResult {
   private var targetRange: NSRange?       { range(named: .target) }
   private var parametersRange: NSRange?   { range(named: .parameters) }
   
-  func eip61Target(in string: String) -> String?       { value(of: .target, in: string) }
-  func eip61Parameters(in string: String) -> String?   { value(of: .parameters, in: string) }
+  func eip67Target(in string: String) -> String?       { value(of: .target, in: string) }
+  func eip67Parameters(in string: String) -> String?   { value(of: .parameters, in: string) }
   
   // MARK: - Private
   
-  private func range(named: EIP61Groups) -> NSRange? {
+  private func range(named: EIP67Groups) -> NSRange? {
     let range = range(withName: named.rawValue)
     guard range.location != NSNotFound, range.length > 0 else { return nil }
     return range
   }
   
-  private func value(of rangeName: EIP61Groups, in string: String) -> String? {
+  private func value(of rangeName: EIP67Groups, in string: String) -> String? {
     guard let nsrange = range(named: rangeName),
           let range = Range(nsrange, in: string) else { return nil }
     return String(string[range])
