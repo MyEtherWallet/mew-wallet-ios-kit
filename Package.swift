8dc154cb5886cd43a4e086c55d76fd24effa1693
Misha Nikanorov <fobozsb@gmail.com>
2021-10-30 15:59:07 -0700

    feature/package_update: Update packages and dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,11 +16,11 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.3.8")),
-    .package(url: "https://github.com/MyEtherWallet/MEW-wallet-iOS-secp256k1-package.git", from: "1.0.0"),
+    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.4.2")),
+    .package(url: "https://github.com/MyEtherWallet/MEW-wallet-iOS-secp256k1-package.git", .exact("1.0.1")),
     .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
     .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
-    .package(url: "https://github.com/MyEtherWallet/bls-eth-swift.git", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/MyEtherWallet/bls-eth-swift.git", .branch("feature/package_update")), // .exact("1.0.0")),
     .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
     .package(name: "MEWwalletTweetNacl", url: "https://github.com/MyEtherWallet/mew-wallet-ios-tweetnacl.git", .upToNextMajor(from: "1.0.0"))
   ],
